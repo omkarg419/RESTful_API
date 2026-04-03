66f955062b529b32556ad092eac406f6d0060f3b
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-04-03 16:38:09 +0530

    feat: update package.json and server.js for module support; add user model schema

--- REST_API_JS_Mongoose_MongoDB/server.js
+++ REST_API_JS_Mongoose_MongoDB/server.js
@@ -1,17 +1,17 @@
 import "dotenv/config";
-import app from "./src/app";
-import connectDB from "./src/common/config/db";
+import app from "./src/app.js";
+import connectDB from "./src/common/config/db.js";
 
 const PORT = process.env.PORT || 5000;
 
 const start = async () => {
-    await connectDB()
+	await connectDB();
 	app.listen(PORT, () => {
 		console.log(`Server is running at ${PORT} in ${process.env.NODE_ENV} mode`);
 	});
 };
 
-start().catch((err)=>{
-    console.error("failed to start server")
-    process.exit(1)
-})
+start().catch((err) => {
+	console.error("failed to start server",err);
+	process.exit(1);
+});

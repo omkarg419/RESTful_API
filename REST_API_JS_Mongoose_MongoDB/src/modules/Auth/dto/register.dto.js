e9db517f6b4d5166bddafbae53ffc2d3725cc82d
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-04-27 14:15:05 +0530

    refactor(dto): format password and role validation for better readability

--- REST_API_JS_Mongoose_MongoDB/src/modules/Auth/dto/register.dto.js
+++ REST_API_JS_Mongoose_MongoDB/src/modules/Auth/dto/register.dto.js
@@ -5,9 +5,13 @@ class RegisterDto extends BaseDto {
 	static schema = Joi.object({
 		name: Joi.string().min(2).max(50).required(),
 		email: Joi.string().email().required().lowercase(),
-		password: Joi.string().min(8).required().max(100).message("Password must be between 8 and 100 characters long"),
-        role: Joi.string().valid("customer", "seller").default("customer"),
+		password: Joi.string()
+			.min(8)
+			.required()
+			.max(100)
+			.message("Password must be between 8 and 100 characters long"),
+		role: Joi.string().valid("customer", "seller").default("customer"),
 	});
 }
 
-export default RegisterDto; 
+export default RegisterDto;

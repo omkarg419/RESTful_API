aeba915d06199544166b4d65011c62393ae67404
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-04-28 01:28:59 +0530

    refactor(auth): streamline refreshAccessToken and getMe functions for improved readability

--- REST_API_JS_Mongoose_MongoDB/src/modules/Auth/auth.controller.js
+++ REST_API_JS_Mongoose_MongoDB/src/modules/Auth/auth.controller.js
@@ -19,27 +19,40 @@ const login = async (req, res) => {
 };
 
 const refreshAccessToken = async (req, res) => {
-    const { refreshToken } = req.cookies;
-    const { user, accessToken, refreshToken: newRefreshToken } = await authService.refreshAccessToken(refreshToken);
-    res.cookie("refreshToken", newRefreshToken, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    });
-    ApiResponse.ok(res, "Access token refreshed successfully", { user, accessToken });
+	const refreshToken =
+		req.cookies?.refreshToken || // web (cookie)
+		req.headers.authorization?.split(" ")[1]; // mobile (header);
+
+	if (!refreshToken) {
+		return ApiError.unauthorized("Refresh token required");
+	}
+
+	const {
+		user,
+		accessToken,
+		refreshToken: newRefreshToken,
+	} = await authService.refreshAccessToken(refreshToken);
+	res.cookie("refreshToken", newRefreshToken, {
+		httpOnly: true,
+		secure: true,
+		sameSite: "strict",
+		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+	});
+	ApiResponse.ok(res, "Access token refreshed successfully", {
+		user,
+		accessToken,
+	});
 };
 
-const logout = async (req,res) =>{
-    await authService.logout(req.user.id);
-    res.clearCookie("refreshToken");
-    ApiResponse.ok(res, "User logged out successfully");
-}
+const logout = async (req, res) => {
+	await authService.logout(req.user.id);
+	res.clearCookie("refreshToken");
+	ApiResponse.ok(res, "User logged out successfully");
+};
 
 const getMe = async (req, res) => {
-    const user = await authService.getMe(req.user.id);
-    ApiResponse.ok(res, "User profile fetched successfully", user);
-}
-
+	const user = await authService.getMe(req.user.id);
+	ApiResponse.ok(res, "User profile fetched successfully", user);
+};
 
 export { register, login, refreshAccessToken, logout, getMe };

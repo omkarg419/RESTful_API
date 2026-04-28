97ffd715867ec84e25593343d1b0ae08d2de44aa
omkarg419 <68603231+omkarg419@users.noreply.github.com>
2026-04-28 14:29:08 +0530

    fix(app): remove unused cookie import to clean up code

--- REST_API_JS_Mongoose_MongoDB/src/app.js
+++ REST_API_JS_Mongoose_MongoDB/src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import AuthRouter from "./modules/Auth/auth.routes.js";
-import cookie from "cookie-parser";
 import cookieParser from "cookie-parser";
 
 const app = express();
